a087f8db83661bb757ebde39c00f25f7141e52e2
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2024-08-07 21:12:56 -0300

    Rotas HTTPs das fotos

---
+++ backend/routes/PhotoRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+// Controller
+const {
+  insertPhoto,
+  deletePhoto,
+  getAllPhotos,
+  getUserPhotos,
+  getPhotoById,
+  updatePhoto,
+  likePhoto,
+} = require("../controllers/PhotoController");
+
+// Milddwares
+const authGuard = require("../middlewares/authGuard");
+const validate = require("../middlewares/handleValidation");
+const {
+  photoInsertValidation,
+  photoUpdateValidation,
+} = require("../middlewares/photoValidation");
+const { imageUpload } = require("../middlewares/imageUpload");
+
+// Routes
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
+router.delete("/:id", authGuard, deletePhoto);
+router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
+router.put("/like/:id", authGuard, likePhoto);
+
+module.exports = router;
