3e662a4002c556ec6a783eeec736b5ec383388e5
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2024-08-14 22:32:23 -0300

    Funcionalidade de busca de foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -202,6 +202,16 @@ const commentPhoto = async (req, res) => {
   });
 };
 
+// Search photos by title
+
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+
+  res.status(200).json(photos);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -211,4 +221,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 };
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -11,6 +11,7 @@ const {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 } = require("../controllers/PhotoController");
 
 // Milddwares
@@ -35,6 +36,8 @@ router.post(
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/search", authGuard, searchPhotos);
+
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
