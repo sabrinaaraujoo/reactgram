f481fe82a96317fdb0f44b2020cbdf23ee04312c
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2024-09-17 21:04:49 -0300

    Slice do login

--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -30,6 +30,18 @@ export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
+// Sing in an user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  const data = await authService.login(user);
+
+  // Check for errors
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -37,7 +49,7 @@ export const authSlice = createSlice({
     reset: (state) => {
       state.loading = false;
       state.error = false;
-      state.success = false;
+      state.sucess = false;
     },
   },
   extraReducers: (builder) => {
@@ -48,7 +60,7 @@ export const authSlice = createSlice({
       })
       .addCase(register.fulfilled, (state, action) => {
         state.loading = false;
-        state.success = true;
+        state.sucess = true;
         state.error = null;
         state.user = action.payload;
       })
@@ -59,8 +71,23 @@ export const authSlice = createSlice({
       })
       .addCase(logout.fulfilled, (state, action) => {
         state.loading = false;
-        state.success = true;
+        state.sucess = true;
         state.error = null;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.sucess = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
         state.user = null;
       });
   },
