26448e859ec86cd1366fbe50c59045722bb25575
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2024-09-22 16:14:48 -0300

    Criando-página de postagem de foto

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import "./Profile.css";
+
+import { uploads } from "../../utils/config";
+
+// Components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPenFill, BsXLg } from "react-icons/bs";
+
+// Hooks
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
